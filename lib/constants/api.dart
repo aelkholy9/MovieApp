c329667cfe12ce9fbf62f1b8a59e04ec7d6a650d
elkholy <boodykh@gmail.com>
2022-03-11 20:54:32 +0200

    added constant urls for authentication

---
+++ lib/constants/api.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  static const String apiKey = "31521ab741626851b73c684539c33b5a";
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
+  static const String requestTokenUrl = 'authentication/token/new';
+  static const String validateTokenUrl =
+      'authentication/token/validate_with_login';
+  static const String createSessionUrl = 'authentication/session/new';
+}
